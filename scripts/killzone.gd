7b78d419a2f226bbc3155b4c8193ae25c250b89d
Michael Foley <foley3@gmail.com>
2025-08-18 00:56:08 -0700

    Play killzone sound

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -1,9 +1,10 @@
 extends Area2D
 
 @onready var timer = $Timer
+@onready var audio_player = $AudioPlayer
 
 func _on_body_entered(body: Node2D) -> void:
-	
+	audio_player.play()
 	timer.start()
 
 func _on_timer_timeout() -> void:
